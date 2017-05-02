1f124fd729ef18ab63f7f9acc672860fbe5a10c1
Hendrik Brandt <heb@20khz.de>
2017-05-02 17:56:40 +1000

    Fix autowiring of BeatOutput

--- src/main/java/io/github/hebra/elasticsearch/beat/meterbeat/akka/FetchActor.java
+++ src/main/java/io/github/hebra/elasticsearch/beat/meterbeat/akka/FetchActor.java
@@ -1,6 +1,6 @@
 /**
- * (C) 2016-2017 Hendrik Brandt <https://github.com/hebra/> This file is part of MeterBeat. MeterBeat is free software: you
- * can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free
+ * (C) 2016-2017 Hendrik Brandt <https://github.com/hebra/> This file is part of MeterBeat. MeterBeat is free software:
+ * you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option) any later version. MeterBeat is distributed
  * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a
@@ -13,8 +13,8 @@ import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -36,6 +36,9 @@ public class FetchActor extends UntypedActor
 	private ActorSystem actorSystem;
 
 	@Autowired
+	private ApplicationContext context;
+
+	@Autowired
 	private SpringExtension springExtension;
 
 	@Autowired
@@ -48,7 +51,8 @@ public class FetchActor extends UntypedActor
 	{
 		if ( pushActor == null )
 		{
-			pushActor = actorSystem.actorOf( springExtension.props( "pushOutputActor" ),
+			pushActor = actorSystem.actorOf(
+					springExtension.props( "pushOutputActor" ),
 					"push-".concat( UUID.randomUUID().toString() ) );
 
 		}
@@ -58,9 +62,13 @@ public class FetchActor extends UntypedActor
 			final IDevice device = ( IDevice ) message;
 
 			Optional.ofNullable( device.fetchData() ).ifPresent( watt -> {
-				pushActor.tell( new BeatOutput()
-						.beat( Beat.fromConfig( device.config() ) )
-						.power( new Power( watt ) ), getSelf() );
+
+				BeatOutput output = new BeatOutput();
+				context.getAutowireCapableBeanFactory().autowireBean( output );
+
+				pushActor
+						.tell( output.beat( Beat.fromConfig( device.config() ) )
+								.power( new Power( watt ) ), getSelf() );
 			} );
 
 			actorSystem.scheduler().scheduleOnce(
--- src/main/java/io/github/hebra/elasticsearch/beat/meterbeat/output/BeatOutput.java
+++ src/main/java/io/github/hebra/elasticsearch/beat/meterbeat/output/BeatOutput.java
@@ -1,6 +1,6 @@
 /**
- * (C) 2016-2017 Hendrik Brandt <https://github.com/hebra/> This file is part of MeterBeat. MeterBeat is free software: you
- * can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free
+ * (C) 2016-2017 Hendrik Brandt <https://github.com/hebra/> This file is part of MeterBeat. MeterBeat is free software:
+ * you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option) any later version. MeterBeat is distributed
  * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -30,6 +31,7 @@ import lombok.experimental.Accessors;
  */
 @Accessors( fluent = true )
 @Component
+@Scope( "prototype" )
 public class BeatOutput
 {
 	private static final Logger LOGGER = LoggerFactory
